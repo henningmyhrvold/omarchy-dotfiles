2c8ff9aa6870e2fe0d9738606023a0f8abc81912
Henning Myhrvold <henning.myhrvold@gmail.com>
2026-02-07 12:33:26 +0100

    Initial commit

---
+++ omarchy-mods.sh
@@ -0,0 +1,5 @@
+
+curl -fsSL https://imbypass.github.io/omarchy-theme-hook/install.sh | bash
+omarchy-theme-install https://github.com/abhijeet-swami/omarchy-spectra-theme
+omarchy-theme-install https://github.com/
+
