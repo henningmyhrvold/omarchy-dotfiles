e19bdc2086c9cd9d8e2ad9b4f0ed2baf500c64ee
Henning Myhrvold <henning.myhrvold@gmail.com>
2026-02-07 17:28:26 +0100

    Initial commit

--- omarchy-mods.sh
+++ omarchy-mods.sh
@@ -4,6 +4,27 @@ set -e
 
 # Theme hook for additional theme support
 curl -fsSL https://imbypass.github.io/omarchy-theme-hook/install.sh | bash
+sleep 2
+curl -fsSL https://raw.githubusercontent.com/JacobusXIII/omarchy-wireguard-vpn-toggle/main/install.sh | bash
+sleep 2
 
 # Install extra themes
+omarchy-theme-install https://github.com/abhijeet-swami/omarchy-ayaka-theme
+sleep 2
+omarchy-theme-install https://github.com/JaxonWright/omarchy-midnight-theme
+sleep 2
+omarchy-theme-install https://github.com/bjarneo/omarchy-pulsar-theme
+sleep 2
 omarchy-theme-install https://github.com/abhijeet-swami/omarchy-spectra-theme
+sleep 2
+
+
+#
+#
+#
+#
+#
+#
+#
+#
+#
