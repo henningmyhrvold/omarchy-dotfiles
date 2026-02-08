8f99ff57f741e8170a31c61c83a9b61d26da3e44
Henning Myhrvold <henning.myhrvold@gmail.com>
2026-02-08 12:21:21 +0100

    Initial commit

--- scripts/omarchy-mods-first-time.sh
+++ scripts/omarchy-mods-first-time.sh
@@ -3,20 +3,20 @@ set -e
 # One-time Omarchy customizations — run once by bootstrap.sh
 
 # Theme hook for additional theme support
-curl -fsSL https://imbypass.github.io/omarchy-theme-hook/install.sh | bash
-sleep 2
+#curl -fsSL https://imbypass.github.io/omarchy-theme-hook/install.sh | bash
+#sleep 2
 curl -fsSL https://raw.githubusercontent.com/JacobusXIII/omarchy-wireguard-vpn-toggle/main/install.sh | bash
-sleep 2
+sleep 3
 
 # Install extra themes
 omarchy-theme-install https://github.com/abhijeet-swami/omarchy-ayaka-theme
-sleep 2
+sleep 3
 omarchy-theme-install https://github.com/JaxonWright/omarchy-midnight-theme
-sleep 2
+sleep 3
 omarchy-theme-install https://github.com/bjarneo/omarchy-pulsar-theme
-sleep 2
+sleep 3
 omarchy-theme-install https://github.com/abhijeet-swami/omarchy-spectra-theme
-sleep 2
+sleep 3
 
 
 #
